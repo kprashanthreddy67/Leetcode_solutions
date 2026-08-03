3fa602d90659fe95af8235365ea470fe6f203c3f
kprashanthreddy67 <kprashanthreddy75@gmail.com>
2026-08-03 21:06:28 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (5.37%) - LeetSync

---
+++ 1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int ans=0;
+    public void check(TreeNode root,int min,int max){
+        if(root==null){
+            return;
+        }
+        ans=Math.max(ans,Math.abs(root.val-min));
+        ans=Math.max(ans,Math.abs(root.val-max));
+        min=Math.min(min,root.val);
+        max=Math.max(max,root.val);
+        check(root.left,min,max);
+        check(root.right,min,max);
+        // return ans;
+    }
+    public int maxAncestorDiff(TreeNode root) {
+         check(root,root.val,root.val);
+        return ans;
+    }
+}
