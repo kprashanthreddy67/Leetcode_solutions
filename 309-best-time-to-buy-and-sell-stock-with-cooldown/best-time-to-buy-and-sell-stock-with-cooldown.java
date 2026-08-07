54532479a7ec324d4691fab62645f9ea7c260ca6
kprashanthreddy67 <kprashanthreddy75@gmail.com>
2026-08-07 22:04:07 +0530

    Time: 1 ms (70.25%) | Memory: 43.5 MB (49.45%) - LeetSync

---
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int check(int i,int buy,int prices[],int dp[][]){
+        if(i>=prices.length){
+            return 0;
+        }
+        if(dp[i][buy]!=-1){
+            return dp[i][buy];
+        }
+        int profit=0;
+        if(buy==1){
+            profit=Math.max(-prices[i]+check(i+1,0,prices,dp),0+check(i+1,1,prices,dp));
+        }else{
+            if(i<prices.length){
+            profit=Math.max(prices[i]+check(i+2,1,prices,dp),0+check(i+1,0,prices,dp));
+            }
+        }
+        return dp[i][buy]= profit;
+    }
+    public int maxProfit(int[] prices) {
+        int dp[][]=new int[prices.length][2];
+        for(int i=0;i<prices.length;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        return check(0,1,prices,dp);
+    }
+}
