9a414a5ea3835b77643a7b05c6b27877cd952488
kprashanthreddy67 <kprashanthreddy75@gmail.com>
2026-08-07 22:35:51 +0530

    Time: 54 ms (38.97%) | Memory: 117.5 MB (38.59%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.java
@@ -15,12 +15,37 @@ class Solution {
         return dp[i][buy][cap] = profit;
     }
     public int maxProfit(int[] prices) {
-        int dp[][][]=new int[prices.length][2][3];
-        for(int i=0;i<prices.length;i++){
-             for(int j=0;j<=1;j++){
-                Arrays.fill(dp[i][j],-1);
-             }
+        int n=prices.length;
+        int dp[][][]=new int[prices.length+1][2][3];
+        for(int buy=0;buy<=1;buy++){
+            for(int cap=1;cap<=2;cap++){
+                dp[n][buy][cap]=0;
+            }
         }
-        return check(0,1,2,prices,dp);
+        for(int i=0;i<prices.length;i++){
+            for(int buy=0;buy<=1;buy++){
+                dp[i][buy][0]=0;
+            }
+        }
+        for(int i=prices.length-1;i>=0;i--){
+            for(int buy=0;buy<=1;buy++){
+                for(int cap=1;cap<=2;cap++){
+                    int profit=0;
+                    if(buy==1){
+                        profit=Math.max(-prices[i]+dp[i+1][0][cap],0+dp[i+1][1][cap]);
+                    }else{
+                        profit=Math.max(prices[i]+dp[i+1][1][cap-1],0+dp[i+1][0][cap]);
+                    }
+                    dp[i][buy][cap]=profit;
+                }
+            }
+        }
+        return dp[0][1][2];
+        // for(int i=0;i<prices.length;i++){
+        //      for(int j=0;j<=1;j++){
+        //         Arrays.fill(dp[i][j],-1);
+        //      }
+        // }
+        // return check(0,1,2,prices,dp);
     }
 }
