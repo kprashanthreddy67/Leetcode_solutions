b41ec11243d007223f411e18f04f7e968400d3d1
kprashanthreddy67 <kprashanthreddy75@gmail.com>
2026-08-16 10:57:00 +0530

    Time: 17 ms (30.35%) | Memory: 57 MB (35.14%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int check(int i,int coins[],int amount,int dp[][]){
+        if(i==0){
+            if(amount%coins[0]==0){
+                return 1;
+            }else{
+                return 0;
+            }
+        }
+        if(dp[i][amount]!=-1){
+            return  dp[i][amount];
+        }
+        int not=check(i-1,coins,amount,dp);
+        int take=0;
+        if(amount>=coins[i]){
+            take=check(i,coins,amount-coins[i],dp);
+        }
+        return dp[i][amount]= not+take;
+    }
+    public int change(int amount, int[] coins) {
+        int dp[][]=new int[coins.length][amount+1];
+        for(int i=0;i<coins.length;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        return check(coins.length-1,coins,amount,dp);
+
+    }
+}
