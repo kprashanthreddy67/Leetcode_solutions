66e8efec0b921dc2d42467f94fedf7e7eb22e776
kprashanthreddy67 <kprashanthreddy75@gmail.com>
2026-08-16 18:34:21 +0530

    Time: 27 ms (27.61%) | Memory: 46.9 MB (35.51%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,11 +1,10 @@
 class Solution {
-    public int check(int i,int[] coins,int amount,int dp[][]){
+    public int check(int i,int coins[],int amount,int dp[][]){
         if(i==0){
             if(amount%coins[0]==0){
                 return amount/coins[0];
-            }else{
-                return (int)1e9;
             }
+            return (int)1e9;
         }
         if(dp[i][amount]!=-1){
             return dp[i][amount];
@@ -22,7 +21,7 @@ class Solution {
         for(int i=0;i<coins.length;i++){
             Arrays.fill(dp[i],-1);
         }
-        int ans= check(coins.length-1,coins,amount,dp);
+        int ans=check(coins.length-1,coins,amount,dp);
         if(ans>=(int)1e9){
             return -1;
         }
