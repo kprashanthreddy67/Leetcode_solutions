1cbe845cda7bd9af78f2d66dcd9c8d80a552b26c
kprashanthreddy67 <kprashanthreddy75@gmail.com>
2026-08-17 21:28:48 +0530

    Time: 1000 ms (32.57%) | Memory: 0B (100.00%) - LeetSync

---
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+select Department,Employee,Salary from 
+(select d.name as Department,e.name as Employee,e.salary as Salary,
+dense_rank() over(partition by d.name
+                  order by e.salary desc) as   ranks
+ from Employee e
+ join Department d
+ on e.departmentId=d.id) as t
+ where ranks=1;
