c08b5a688dc912610efa6ae9b1b86b4754386036
kprashanthreddy67 <kprashanthreddy75@gmail.com>
2026-08-18 21:59:29 +0530

    Time: 904 ms (92.67%) | Memory: 0B (100.00%) - LeetSync

--- 185-department-top-three-salaries/department-top-three-salaries.sql
+++ 185-department-top-three-salaries/department-top-three-salaries.sql
@@ -1,6 +1,6 @@
 # Write your MySQL query statement below
-select Department,Employee, Salary from(
-    select d.name as Department, e.name as Employee,e.salary as Salary,
+select Department,Employee,Salary from(
+    select d.name as Department,e.name as Employee,e.salary as salary,
     dense_rank() over(partition by d.name order by e.salary desc) as ranks
     from Employee e
     join Department d
