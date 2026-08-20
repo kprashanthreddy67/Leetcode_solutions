68c178cbe2e0a19824800eba9c13084d6b5443ea
kprashanthreddy67 <kprashanthreddy75@gmail.com>
2026-08-20 17:35:42 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (54.55%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.java
+++ 486-predict-the-winner/predict-the-winner.java
@@ -1,16 +1,11 @@
 class Solution {
-
     public int check(int i,int j,int[] nums,int dp[][]){
         if(i==j){
             return nums[i];
-
         }
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
-        // int left=nums[i]-check(i+1,j,nums);
-        // int right=nums[j]-check(i,j-1,nums);
-        // return Math.max(left,right);
         return dp[i][j]= Math.max(nums[i]-check(i+1,j,nums,dp),nums[j]-check(i,j-1,nums,dp));
     }
     public boolean predictTheWinner(int[] nums) {
@@ -19,6 +14,7 @@ class Solution {
             Arrays.fill(dp[i],-1);
         }
         int ans=check(0,nums.length-1,nums,dp);
+        
         if(ans<0){
             return false;
         }
