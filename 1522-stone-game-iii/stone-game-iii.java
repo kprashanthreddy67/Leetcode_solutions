2ead7f45151ba3f99c34d561d40758c964debf59
kprashanthreddy67 <kprashanthreddy75@gmail.com>
2026-08-25 18:50:43 +0530

    Time: 34 ms (17.48%) | Memory: 93.5 MB (11.53%) - LeetSync

---
+++ 1522-stone-game-iii/stone-game-iii.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int check(int i,int[] stoneValue,int dp[]){
+        if(i>=stoneValue.length){
+            return 0;
+        }
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+        int result=Integer.MIN_VALUE;
+        result=Math.max(result,stoneValue[i]-check(i+1,stoneValue,dp));
+        if(i+1<stoneValue.length){
+         result=Math.max(result,stoneValue[i]+stoneValue[i+1]-check(i+2,stoneValue,dp));
+        }
+        if(i+2<stoneValue.length){
+            result=Math.max(result,stoneValue[i]+stoneValue[i+1]+stoneValue[i+2]-check(i+3,stoneValue,dp));
+        }
+        return dp[i]= result;
+    }
+    public String stoneGameIII(int[] stoneValue) {
+        int dp[]=new int[stoneValue.length+1];
+        Arrays.fill(dp,-1);
+        int ans=check(0,stoneValue,dp);
+        // String
+        if(ans>0){
+            return "Alice";
+        }else if(ans<0){
+            return "Bob";
+        }else{
+            return "Tie";
+        }
+    }
+}
