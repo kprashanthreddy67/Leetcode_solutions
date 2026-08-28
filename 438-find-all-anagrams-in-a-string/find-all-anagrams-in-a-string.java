a23dbbafd086ccedd5115319700fcfa54ac23080
kprashanthreddy67 <kprashanthreddy75@gmail.com>
2026-08-29 01:04:07 +0530

    Time: 63 ms (20.12%) | Memory: 47.7 MB (15.14%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,28 +1,30 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> ans=new ArrayList<>();
         HashMap<Character,Integer> hm1=new HashMap<>();
+        HashMap<Character,Integer> hm2=new HashMap<>();
+        int  l=0;
+        int max=0;
+        int cnt=0;
+        int k=p.length();
+        List<Integer> ans=new ArrayList<>();
         for(int i=0;i<p.length();i++){
             char ch=p.charAt(i);
             hm1.put(ch,hm1.getOrDefault(ch,0)+1);
         }
-        int l=0;
-        int k=p.length();
-        HashMap<Character,Integer> hm2=new HashMap<>();
         for(int r=0;r<s.length();r++){
             char ch=s.charAt(r);
             hm2.put(ch,hm2.getOrDefault(ch,0)+1);
             if(r-l+1>k){
-                char rem=s.charAt(l);
-                hm2.put(rem,hm2.get(rem)-1);
-                if(hm2.get(rem)==0){
-                    hm2.remove(rem);
+                char ch1=s.charAt(l);
+                hm2.put(ch1,hm2.get(ch1)-1);
+                if(hm2.get(ch1)==0){
+                    hm2.remove(ch1);
                 }
                 l++;
             }
             if(r-l+1==k){
                 if(hm1.equals(hm2)){
-                ans.add(l);
+                    ans.add(l);
                 }
             }
         }
