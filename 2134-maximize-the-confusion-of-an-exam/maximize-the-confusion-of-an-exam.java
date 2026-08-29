e7a7834efa1fd6dfed62318aff8fc1a1e59fd154
kprashanthreddy67 <kprashanthreddy75@gmail.com>
2026-08-29 14:07:09 +0530

    Time: 12 ms (80.85%) | Memory: 46.4 MB (46.76%) - LeetSync

---
+++ 2134-maximize-the-confusion-of-an-exam/maximize-the-confusion-of-an-exam.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int maxConsecutiveAnswers(String answerKey, int k) {
+        int l=0;
+        int max=0;
+        int tr=0;
+        int fl=0;
+        int cnt=0;
+        int min=0;
+        for(int r=0;r<answerKey.length();r++){
+            char ch=answerKey.charAt(r);
+            if(ch=='T'){
+                tr++;
+            }else{
+                fl++;
+            }
+            while(Math.min(tr,fl)>k){
+                char ch1=answerKey.charAt(l);
+                if(ch1=='T'){
+                    tr--;
+                }else{
+                    fl--;
+                }
+                l++;
+            }
+            max=Math.max(max,r-l+1);
+        }
+        return max;
+    }
+}
