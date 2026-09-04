b839dd8a9a3a16d3506b6d25097141c6d1af7290
kprashanthreddy67 <kprashanthreddy75@gmail.com>
2026-09-04 10:55:01 +0530

    Time: 28 ms (24.91%) | Memory: 47 MB (25.76%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -3,25 +3,28 @@ class Solution {
         if(i==0){
             if(amount%coins[0]==0){
                 return amount/coins[0];
+            }else{
+                return (int)1e9;
             }
-            return (int)1e9;
         }
-          if(dp[i][amount]!=-1){
+        if(dp[i][amount]!=-1){
             return dp[i][amount];
-          }
+        }
         int not=check(i-1,coins,amount,dp);
         int take=Integer.MAX_VALUE;
         if(amount>=coins[i]){
             take=1+check(i,coins,amount-coins[i],dp);
         }
+
         return dp[i][amount]= Math.min(not,take);
     }
     public int coinChange(int[] coins, int amount) {
         int dp[][]=new int[coins.length][amount+1];
         for(int i=0;i<coins.length;i++){
+
             Arrays.fill(dp[i],-1);
         }
-        int ans= check(coins.length-1,coins,amount,dp);
+        int ans=check(coins.length-1,coins,amount,dp);
         if(ans>=(int)1e9){
             return -1;
         }
