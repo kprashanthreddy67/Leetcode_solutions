119cef698cb82a93e8d16f04871ade920e0566c8
kprashanthreddy67 <kprashanthreddy75@gmail.com>
2026-09-05 17:20:01 +0530

    Time: 5 ms (66.41%) | Memory: 126.8 MB (59.92%) - LeetSync

--- 4285-smallest-stable-index-ii/smallest-stable-index-ii.java
+++ 4285-smallest-stable-index-ii/smallest-stable-index-ii.java
@@ -1,21 +1,33 @@
 class Solution {
     public int firstStableIndex(int[] nums, int k) {
         int min=Integer.MAX_VALUE;
-        int minfrom[]=new int[nums.length];
+        int minArray[]=new int[nums.length];
         for(int i=nums.length-1;i>=0;i--){
             min=Math.min(min,nums[i]);
-            minfrom[i]=min;
+            minArray[i]=min;
         }
         int max=0;
+        int  maxArray[]=new int[nums.length];
         for(int i=0;i<nums.length;i++){
             max=Math.max(max,nums[i]);
-           int min1=minfrom[i];
-            int ans=max-min1;
+            maxArray[i]=max;
+        }
+        for(int i=0;i<nums.length;i++){
+            int ans=maxArray[i]-minArray[i];
             if(ans<=k){
                 return i;
             }
         }
         return -1;
+        // for(int i=0;i<nums.length;i++){
+        //     max=Math.max(max,nums[i]);
+        //    int min1=minfrom[i];
+        //     int ans=max-min1;
+        //     if(ans<=k){
+        //         return i;
+        //     }
+        // }
+        // return -1;
         //brute force
         // for(int i=0;i<nums.length;i++){
         //     int max=0;
