3094c1b4eafcaaa3d41da159ec7c6890c4371a98
kprashanthreddy67 <kprashanthreddy75@gmail.com>
2026-09-13 22:53:21 +0530

    Time: 49 ms (56.23%) | Memory: 50.6 MB (5.60%) - LeetSync

--- 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
@@ -1,8 +1,8 @@
 class Solution {
     public int check(int[] nums,int k){
          HashMap<Integer,Integer> hm=new HashMap<>();
-        int l=0;
         int ans=0;
+        int l=0;
         for(int r=0;r<nums.length;r++){
             int val=nums[r];
             hm.put(val,hm.getOrDefault(val,0)+1);
@@ -14,13 +14,11 @@ class Solution {
                 }
                 l++;
             }
-            
             ans+=r-l+1;
-            
         }
         return ans;
     }
     public int subarraysWithKDistinct(int[] nums, int k) {
-       return check(nums,k)-check(nums,k-1);
+        return check(nums,k)-check(nums,k-1);
     }
 }
