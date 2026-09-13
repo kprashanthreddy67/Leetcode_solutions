fd8216083af58dceb31209f04dfdf5f530fd564e
kprashanthreddy67 <kprashanthreddy75@gmail.com>
2026-09-13 23:48:50 +0530

    Time: 8 ms (71.20%) | Memory: 44.1 MB (93.61%) - LeetSync

--- 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.java
+++ 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.java
@@ -6,9 +6,10 @@ class Solution {
         for(int r=0;r<s.length();r++){
             char ch1=s.charAt(r);
             char ch2=t.charAt(r);
-            sum+=Math.abs(ch1-ch2);
-            while(sum>maxCost){
-                sum-=Math.abs(s.charAt(l)-t.charAt(l));
+            // sum+=Math.abs(ch1-ch2);
+            maxCost-=Math.abs(ch1-ch2);
+            while(maxCost<0){
+                maxCost+=Math.abs(s.charAt(l)-t.charAt(l));
                 l++;
             }
             ans=Math.max(ans,r-l+1);
