3de7fa2b7a2a9b8f4e882537a0deb54b188ca8e0
kprashanthreddy67 <kprashanthreddy75@gmail.com>
2026-09-13 22:41:53 +0530

    Time: 5 ms (87.17%) | Memory: 109.4 MB (60.18%) - LeetSync

--- 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.java
+++ 3213-count-subarrays-where-max-element-appears-at-least-k-times/count-subarrays-where-max-element-appears-at-least-k-times.java
@@ -1,12 +1,12 @@
 class Solution {
     public long countSubarrays(int[] nums, int k) {
-        int l=0;
-        long ans=0;
-        int cnt=0;
         int max=0;
         for(int i=0;i<nums.length;i++){
             max=Math.max(max,nums[i]);
         }
+        int l=0;
+        long ans=0;
+        int cnt=0;
         for(int r=0;r<nums.length;r++){
             int val=nums[r];
             if(val==max){
