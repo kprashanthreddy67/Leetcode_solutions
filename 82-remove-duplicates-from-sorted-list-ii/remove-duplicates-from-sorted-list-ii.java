e4a2595d5a86df61581f8b0a31157ee6edf87d30
kprashanthreddy67 <kprashanthreddy75@gmail.com>
2026-09-18 23:28:03 +0530

    Time: 10 ms (1.77%) | Memory: 45.8 MB (8.60%) - LeetSync

--- 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.java
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.java
@@ -10,21 +10,20 @@
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
-      TreeMap<Integer,Integer> hm=new TreeMap<>();
-      ListNode temp=head;
-      while(temp!=null){
-        hm.put(temp.val,hm.getOrDefault(temp.val,0)+1);
-        temp=temp.next;
-
-      }
-      ListNode dummy=new ListNode(-1);
-      ListNode curr=dummy;
-      for(int boom:hm.keySet()){
-        if(hm.get(boom)==1){
-            curr.next=new ListNode(boom);
-            curr=curr.next; 
+        TreeMap<Integer,Integer> hm=new TreeMap<>();
+        ListNode temp=head;
+        while(temp!=null){
+            hm.put(temp.val,hm.getOrDefault(temp.val,0)+1);
+            temp=temp.next;
         }
-      }
-      return dummy.next;
+        ListNode dummy=new ListNode(-1);
+        ListNode curr=dummy;
+        for(int boom:hm.keySet()){
+            if(hm.get(boom)==1){
+                curr.next=new ListNode(boom);
+                curr=curr.next;
+            }
+        }
+        return dummy.next;
     }
 }
