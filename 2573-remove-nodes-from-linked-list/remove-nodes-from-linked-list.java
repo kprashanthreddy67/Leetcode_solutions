956a7585246e1bd587dc23b6867a397a1e72e871
kprashanthreddy67 <kprashanthreddy75@gmail.com>
2026-09-19 16:53:38 +0530

    Time: 6 ms (92.63%) | Memory: 130.4 MB (82.16%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.java
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.java
@@ -21,42 +21,21 @@ class Solution {
         return prev;
     }
     public ListNode removeNodes(ListNode head) {
-        List<Integer> ans=new ArrayList<>();
-        ListNode temp=head;
-        while(temp!=null){
-            ans.add(temp.val);
-            temp=temp.next;
-        }
+        head=check(head);
         ListNode dummy=new ListNode(-1);
         ListNode curr=dummy;
+        ListNode temp=head;
         int max=Integer.MIN_VALUE;
-        for(int i=ans.size()-1;i>=0;i--){
-            int val=ans.get(i);
-            if(val>=max){
-                max=val;
-                curr.next=new ListNode(val);
+        while(temp!=null){
+            if(temp.val>=max){
+                max=temp.val;
+                curr.next=temp;
                 curr=curr.next;
             }
+            temp=temp.next;
         }
-        curr = check(dummy.next);
-        return curr;
-        // head=check(head);
-        // ListNode dummy=new ListNode(-1);
-        // ListNode curr=dummy;
-        // ListNode temp=head;
-        // // curr.next=temp;
-        // int max=Integer.MIN_VALUE;
-        // while(temp!=null){
-        //     if(temp.val>=max){
-        //         max=temp.val;
-        //         curr.next=temp;
-        //         curr=curr.next;
-        //         // temp=temp.next;
-        //     }
-        //     temp=temp.next;
-        // }
-        // curr.next=null;
-        // head=check(dummy.next);
-        // return head;
+        curr.next=null;
+        head=check(dummy.next);
+        return head;
     }
 }
