4d6e299497a5e2aada51bc0c766ac11947b4b5b7
kprashanthreddy67 <kprashanthreddy75@gmail.com>
2026-09-19 12:26:01 +0530

    Time: 25 ms (10.54%) | Memory: 193.9 MB (5.18%) - LeetSync

--- 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.java
+++ 3501-delete-nodes-from-linked-list-present-in-array/delete-nodes-from-linked-list-present-in-array.java
@@ -10,27 +10,24 @@
  */
 class Solution {
     public ListNode modifiedList(int[] nums, ListNode head) {
-        List<Integer> ans=new ArrayList<>();
+        HashSet<Integer> hs=new HashSet<>();
+        List<Integer>ans=new ArrayList<>();
         ListNode temp=head;
         while(temp!=null){
             ans.add(temp.val);
-
             temp=temp.next;
-        }
+        } 
         ListNode dummy=new ListNode(-1);
         ListNode curr=dummy;
-        HashSet<Integer> hs=new HashSet<>();
         for(int i=0;i<nums.length;i++){
             hs.add(nums[i]);
         }
         for(int i=0;i<ans.size();i++){
-            int val=ans.get(i);
-            if(!hs.contains(val)){
-                curr.next=new ListNode(val);
+            if(!hs.contains(ans.get(i))){
+                curr.next=new ListNode(ans.get(i));
                 curr=curr.next;
             }
         }
-
         return dummy.next;
     }
 }
