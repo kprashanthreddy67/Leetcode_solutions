8fb9b0ac970bbdc6a748d0cb980ddaa7f7ce8b61
kprashanthreddy67 <kprashanthreddy75@gmail.com>
2026-09-20 22:46:47 +0530

    Time: 6 ms (1.75%) | Memory: 45.7 MB (8.38%) - LeetSync

--- 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.java
+++ 82-remove-duplicates-from-sorted-list-ii/remove-duplicates-from-sorted-list-ii.java
@@ -10,22 +10,37 @@
  */
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
+        // ListNode temp=head;
+        // ListNode dummy=new ListNode(-1);
+        // ListNode curr=dummy;
+        // while(temp!=null){
+        //     if(temp.next!=null && temp.val==temp.next.val){
+        //         while(temp.next!=null && temp.val==temp.next.val){
+        //             temp=temp.next;
+        //         }
+        //         temp=temp.next;
+        //     }else{
+        //         curr.next=temp;
+        //         curr=curr.next;
+        //         temp=temp.next;
+        //     }
+        // }
+        // curr.next=null;
+        // return dummy.next;
+        TreeMap<Integer,Integer>hm=new TreeMap<>();
         ListNode temp=head;
+        while(temp!=null){
+            hm.put(temp.val,hm.getOrDefault(temp.val,0)+1);
+            temp=temp.next;
+        }
         ListNode dummy=new ListNode(-1);
         ListNode curr=dummy;
-        while(temp!=null){
-            if(temp.next!=null && temp.val==temp.next.val){
-                while(temp.next!=null && temp.val==temp.next.val){
-                    temp=temp.next;
-                }
-                temp=temp.next;
-            }else{
-                curr.next=temp;
+        for(int boom:hm.keySet()){
+            if(hm.get(boom)==1){
+                curr.next=new ListNode(boom);
                 curr=curr.next;
-                temp=temp.next;
             }
         }
-        curr.next=null;
         return dummy.next;
     }
 }
