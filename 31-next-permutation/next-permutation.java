f456ab39dff342fb1415865d79146a502f271413
kprashanthreddy67 <kprashanthreddy75@gmail.com>
2026-09-23 17:39:03 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (51.40%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -15,22 +15,20 @@ class Solution {
                 pivot=i;
                 break;
             }
-            
         }
         if(pivot==-1){
-                reverse(nums,pivot+1,nums.length-1);
-                return;
+            reverse(nums,0,nums.length-1);
+            return;
+        }
+        for(int i=nums.length-1;i>pivot;i--){
+            if(nums[i]>nums[pivot]){
+                int temp=nums[pivot];
+                nums[pivot]=nums[i];
+                nums[i]=temp;
+                break;
             }
-            for(int j=nums.length-1;j>pivot;j--){
-                if(nums[j]>nums[pivot]){
-                    int temp=nums[pivot];
-                    nums[pivot]=nums[j];
-                    nums[j]=temp;
-                    break;
-                }
-            }
-            reverse(nums,pivot+1,nums.length-1);
-        
 
+        }
+        reverse(nums,pivot+1,nums.length-1);
     }
 }
